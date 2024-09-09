93b0c5504c5ceb8a61fbf9b483f1016167e0f4e0
Mohammed Essam <115664584+MidoXZ@users.noreply.github.com>
2024-09-09 19:20:12 +0300

    Update urls.py

--- FindMyDoctor/HomePage/urls.py
+++ FindMyDoctor/HomePage/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("Home", views.Home, name="MyDoctorHome"),
+    path("", views.Home, name="MyDoctorHome"),
     path('doctor/<int:id>/', views.Details_Page, name="MyDoctorDetails")
 ]
